f3c3022526ca4cdd8bd687913147d06f4e6a6169
Roxxi-Artz <90343355+Roxxi-Artz@users.noreply.github.com>
2022-02-17 13:14:19 -0500

    Add files via upload

--- trivia challenge/trivia.js
+++ trivia challenge/trivia.js
@@ -1,26 +1,8 @@
-function wrongAnswer(){
-
+function correctAnswerIfElse(){
+    
+    if (condition) {
+        
+    } else {
+        
+    }
 }
-
-
-let counterDisplayElem = document.querySelector('.counter-display');
-let counterMinusElem = document.querySelector('.counter-minus');
-let counterPlusElem = document.querySelector('.counter-plus');
-
-let count = 0;
-
-updateDisplay();
-
-counterPlusElem.addEventListener("click",()=>{
-    count++;
-    updateDisplay();
-}) ;
-
-counterMinusElem.addEventListener("click",()=>{
-    count--;
-    updateDisplay();
-});
-
-function updateDisplay(){
-    counterDisplayElem.innerHTML = count;
-};

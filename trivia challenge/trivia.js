04293125e3266b4f76c1b12d2f51c17086a2257a
Roxxi-Artz <90343355+Roxxi-Artz@users.noreply.github.com>
2022-02-22 12:40:44 -0500

    Add files via upload

--- trivia challenge/trivia.js
+++ trivia challenge/trivia.js
@@ -1,8 +1,57 @@
-function correctAnswerIfElse(){
-    
-    if (condition) {
+
+//Question 1
+function correcta(){
+    if (correcta = "correcta") {
+       document.getElementById("rightOrWrong1").innerHTML = 'True!'
+       document.getElementById("score").innerHTML = 'Score = 1' 
         
-    } else {
+    }
+}
+function incorrecta(){
+    if (incorrecta = "incorrecta") {
+        document.getElementById("rightOrWrong1").innerHTML = 'False!'
         
+    }
+}
+
+//Question 2
+function correctb(){
+    if (correctb = "correctb") {
+        document.getElementById("rightOrWrong2").innerHTML = 'True!'
+        document.getElementById("score").innerHTML = 'Score = 2'
+    }
+}
+function incorrectb(){
+    if (incorrectb = "incorrectb") {
+        document.getElementById("rightOrWrong2").innerHTML = 'False!'
+
+    }
+}
+
+//Question 3
+function correctc(){
+    if (correctc = "correctc") {
+        document.getElementById("rightOrWrong3").innerHTML = 'True!'
+        document.getElementById("score").innerHTML = 'Score = 3'
+    }
+}
+function incorrectc(){
+    if (incorrectc = "incorrectc") {
+        document.getElementById("rightOrWrong3").innerHTML = 'False!'
+
+    }
+}
+
+//Question 4
+function correctd(){
+    if (correctd = "correctd") {
+        document.getElementById("rightOrWrong4").innerHTML = 'True!'
+        document.getElementById("score").innerHTML = 'Score = 4'
+    }
+}
+function incorrectd(){
+    if (incorrectd = "incorrectd") {
+        document.getElementById("rightOrWrong4").innerHTML = 'False!'
+
     }
 }
